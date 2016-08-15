8e7e65e24622dbd8fd52b5eac667cc051a1f3597
wheato <wheato2010@gmail.com>
2016-08-15 21:21:34 +0800

    init

---
+++ src/components/head.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="m-header"><h2>vHour</h2></header>
+</template>
+
+<script>
+    export default {
+        data () {
+            return ''
+        }
+    }
+</script>
+
+<style lang="less">
+    .m-header{
+        width: 100%;
+        height: 45px;
+        background: #000;
+        color: #fff;
+        text-align: center;
+        line-height: 45px;
+    }
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,11 @@
+<template>
+    <ul class="m-object-list">
+
+    </ul>
+</template>
+
+<script>
+
+</script>
+
+<style lang="less"></style>
